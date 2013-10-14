a2f80eb0ef3ef65c91c997b5f103c3622f82bdae
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2013-10-14 18:55:38 +0300

    added <h:selectOneMenu>

--- ChatClientJava/src/playtika/ua/vn/LoginBean.java
+++ ChatClientJava/src/playtika/ua/vn/LoginBean.java
@@ -1,33 +1,53 @@
 package playtika.ua.vn;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
-import java.io.Serializable;
+import javax.faces.model.SelectItem;
 
 @ManagedBean
 @SessionScoped
 public class LoginBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String name;
-	private String pass;
+    private String name;
+    private String pass;
+    private List<SelectItem> users;
 
-	public String getPass() {
-	    return pass;
-	}
+    public LoginBean() {
+	users = new ArrayList<SelectItem>();
+	users.add(new SelectItem(new User("1"),"2"));
+	users.add(new SelectItem(new User("1"),"3"));
+	users.add(new SelectItem(new User("1"),"4"));
+	users.add(new SelectItem(new User("1"),"5"));
+    }
 
-	public void setPass(String pass) {
-	    this.pass = pass;
-	}
+    public String getPass() {
+	return pass;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setPass(String pass) {
+	this.pass = pass;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public List<SelectItem> getUsers() {
+	return users;
+    }
+
+    public void setUsers(List<SelectItem> users) {
+	this.users = users;
+    }
 
 }
---
+++ ChatClientJava/src/playtika/ua/vn/User.java
@@ -0,0 +1,22 @@
+package playtika.ua.vn;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class User {   
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name){
+	this.name = name;
+    }
+}
